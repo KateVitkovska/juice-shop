e53c92a93ca6142d1b8eeaef71ae6bcabd000d57
KateVitkovska <142414338+KateVitkovska@users.noreply.github.com>
2026-03-11 20:09:10 +0200

    Delete features directory

--- features/step-definitions/login.steps.js
+++
@@ -1,7 +0,0 @@
-import { Then } from '@wdio/cucumber-framework';
-
-
-Given(/^I am on the login page$/, async (page) => {
-    await pages[page].open()
-});
-
--- features/step-definitions/registration.steps.js
+++
@@ -1,7 +0,0 @@
-import { Then } from '@wdio/cucumber-framework';
-
-
-Then(/^I press log in$/, async (page) => {
-    await pages[page].open()
-});
-
